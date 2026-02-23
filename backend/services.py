1ef4cba9c422e94d3b24c220e5bb24cd6efb8370
Ujj1225 <ujjwaljha1225@gmail.com>
2026-02-23 07:16:07 +0545

    Services in json format

---
+++ backend/services.py
@@ -0,0 +1,92 @@
+SERVICES = {
+    "loksewa": {
+        "name": "Lok Sewa (PSC)",
+        "photo_size": (200, 230),
+        "photo_max_kb": 100,
+        "doc_max_kb": 500
+    },
+    "passport": {   
+        "name": "e-Passport",
+        "photo_size": (600, 600),
+        "photo_max_kb": 300,
+        "doc_max_kb": 300
+    },
+    "pan_card": {
+        "name": "PAN Card (IRD)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 500,
+        "doc_max_kb": 1024
+    },
+    "ssf": {
+        "name": "Social Security (SSF)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 1024,
+        "doc_max_kb": 1024
+    },
+    "tu_entrance": {
+        "name": "TU Entrance/Exam",
+        "photo_size": (300, 350),
+        "photo_max_kb": 300,
+        "doc_max_kb": 1024
+    },
+    "neb": {
+        "name": "NEB Grade 11/12",
+        "photo_size": (350, 450),
+        "photo_max_kb": 200,
+        "doc_max_kb": 500
+    },
+    "mec": {
+        "name": "Medical Education (MEC)",
+        "photo_size": (200, 230),
+        "photo_max_kb": 200,
+        "doc_max_kb": 200
+    },
+    "ctevt": {
+        "name": "CTEVT Entrance",
+        "photo_size": (350, 450),
+        "photo_max_kb": 300,
+        "doc_max_kb": 500
+    },
+    "nec": {
+        "name": "Engineering Council (NEC)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 200,
+        "doc_max_kb": 1024
+    },
+    "nnc": {
+        "name": "Nursing Council (NNC)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 500,
+        "doc_max_kb": 2048
+    },
+    "nmc": {
+        "name": "Medical Council (NMC)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 500,
+        "doc_max_kb": 2048
+    },
+    "bar_council": {
+        "name": "Bar Council (Law)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 200,
+        "doc_max_kb": 1024
+    },
+    "online_account": {
+        "name": "Online A/C (General)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 500,
+        "doc_max_kb": 2048
+    },
+    "noc": {
+        "name": "No Objection (NOC)",
+        "photo_size": (350, 450),
+        "photo_max_kb": 200,
+        "doc_max_kb": 1024
+    },
+    "standard_pp": {
+        "name": "Standard PP Size",
+        "photo_size": (413, 531),
+        "photo_max_kb": 500,
+        "doc_max_kb": 1024
+    }
+}
