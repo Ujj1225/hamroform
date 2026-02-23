038dada8bb38903df545e9a126e4214c4c992d88
Ujj1225 <ujjwaljha1225@gmail.com>
2026-02-23 14:57:33 +0545

    Added domain to cors

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ app = FastAPI(title="HamroForm API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://hamroform.com"
         "http://localhost:5173",
         "http://127.0.0.1:5173"
     ],
