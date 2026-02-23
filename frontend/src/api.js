f336c7bde38c9407ee5ec3030c3c7a2fb7a01a70
Ujj1225 <ujjwaljha1225@gmail.com>
2026-02-23 15:00:16 +0545

    Added public api for backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://3.110.136.143",
 });
 
 export const processPhoto = (file, serviceKey) => {
