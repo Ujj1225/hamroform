78e8ba0d98470233de5af65b513341cd13c56ad7
Ujj1225 <ujjwaljha1225@gmail.com>
2026-03-04 09:30:54 +0545

    Base api from hugging face

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://ujj1225-backend.hf.space",
 });
 
 export const processPhoto = (file, serviceKey) => {
